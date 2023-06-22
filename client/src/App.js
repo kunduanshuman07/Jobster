4fe8d72b2acfc9211885be882140ba842d29b0c4
Anshuman Kundu <kundu4coding@gmail.com>
2023-06-22 16:43:23 +0530

    Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Landing from './pages/landing'
 const App = () => {
   return (
     <div>
-      
+      <Landing/>
     </div>
   )
 }
---
+++ client/src/pages/landing.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>some text</p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img"/>
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
